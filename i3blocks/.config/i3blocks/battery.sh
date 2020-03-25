d45624e4c6bf8e87f69a9c0775f52e75bbc63a7b
jimDragon <jim.dragons@gmail.com>
2020-03-25 15:26:28 +0200

    fixed-i3-wrong-folder-path

--- i3blocks/.config/i3blocks/battery.sh
+++ i3blocks/.config/i3blocks/battery.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
+BAT=$(acpi -b | grep -E -o -m 1 '[0-9][0-9]?%')
+STATUS=$(acpi -b | awk '{printf $3}')
 
-# Set urgent flag below 5% or use orange below 20%
-if [[ ${BAT%?} -le 30 ]]; then
-  echo " $BAT"
-elif [[ ${BAT%?} -le 65 ]]; then
-  echo " $BAT"
-elif [[ ${BAT%?} -le 80 ]]; then
-  echo " $BAT"
-else
-  echo " $BAT"
+if [[ ${STATUS} == "Discharging," ]]; then
+  if [[ ${BAT%?} -le 15 ]]; then
+    echo " $BAT"
+    echo " $BAT"   # Adding it a second for the hex color to apply
+    echo "#FF0000"
+  elif [[ ${BAT%?} -le 30 ]]; then
+    echo " $BAT"
+  elif [[ ${BAT%?} -le 65 ]]; then
+    echo " $BAT"
+  elif [[ ${BAT%?} -le 80 ]]; then
+   echo " $BAT"
+  else
+    echo " $BAT"
+  fi
+elif [[ ${STATUS} == "Charging," ]]; then
+  echo " $BAT"
+elif [[ $STATUS == "Full," ]]; then
+  echo "FULL 100%"
 fi
 
 exit 0
