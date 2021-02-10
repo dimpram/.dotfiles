fab4b0f45b641c17fc560a957158c8c724764143
jimDragon <jim.dragons@gmail.com>
2021-02-10 20:16:28 +0200

    Configured more programs + README.md

--- .local/bin/installenv.sh
+++ .local/bin/installenv.sh
@@ -22,6 +22,7 @@ setfont ter-v32n
 # Stowing
 echo -e "\nStowing Dotfiles\n"
 mkdir -p $HOME/.config                      # Make the config folder so stowing everything doesn't just symlink the folder
+mkdir -p $HOME/.local                       # Make the config folder so stowing everything doesn't just symlink the folder
 rm -f $HOME/.bashrc $HOME/.bash_profile     # Sometimes there's a bashrc or a bash_profile
 sudo pacman -S stow --noconfirm --needed    # Installing stow
 cd $HOME/git
@@ -38,7 +39,7 @@ echo -e "\nInstalling AUR Dependencies\n"
 sudo pacman -S --noconfirm vim base-devel --needed  # Installing base-devel if it's not installed already (This package contains everything required to build from the AUR)
 
 # Build all packages
-for PACKAGE in ttf-merriweather ttf-merriweather-sans ttf-oswald ttf-quintessential ttf-signika ttf-google-fonts-git brave-bin simple-mtpfs
+for PACKAGE in ttf-merriweather ttf-merriweather-sans ttf-oswald ttf-quintessential ttf-signika ttf-google-fonts-git brave-bin simple-mtpfs gruvbox-dark-gtk neovim-nightly-bin
 do
   if [ ! -d "$HOME/git/$PACKAGE/" ]
   then
@@ -60,7 +61,6 @@ sudo pacman -S --noconfirm --needed \
   xcalib \
   ttf-font-awesome \
   libnotify dunst \
-  neovim \
   vifm nautilus \
   openssh \
   htop \
@@ -100,9 +100,11 @@ cd $HOME
 
 startx
 
-# Intel microcode 
 # Install intel driver
 # sudo pacman -S xf86-video-intel
+
+# sudo pacman -S lxappearance
+# Enable it with lxappearance
 
 
 # Blacklist nvidia kernel modules
