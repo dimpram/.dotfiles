c216da592198ee404ebb136b1514c5b61ef7d4d5
dimprama <dev@dimitrisprama.com>
2021-08-06 16:12:27 +0300

    Optimized picom for dwm

--- .local/src/dwm/config.def.h
+++ .local/src/dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 30;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
